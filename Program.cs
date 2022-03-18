5e661271c5d99cde7b1fca51905ed3e20bf14aad
habibmirsyad <habmirs@gmail.com>
2022-03-18 11:04:04 +0700

    menambah class DataGeneric

--- Program.cs
+++ Program.cs
@@ -6,10 +6,22 @@ using System.Threading.Tasks;
 
 namespace tpModul4_1302201138
 {
+    class DataGeneric<T>
+    {
+        public T data;
+    
+        public void printData()
+        {
+            Console.WriteLine("Data yang tersimpan adalah: "+data);
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
+            DataGeneric<string> gen = new DataGeneric<string>();
+            gen.data = "1302201138";
+            gen.printData();
         }
     }
 }
